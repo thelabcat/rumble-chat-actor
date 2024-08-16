4b83790b5c17eb9acac2078f802979ed092195aa
Wilbur Jaywright <zargulthewizard@outlook.com>
2024-08-16 10:31:15 -0400

    Removed js2py usage (see #20)
    Js2py is broken and cannot be used

--- src/rumchat_actor/utils.py
+++ src/rumchat_actor/utils.py
@@ -6,16 +6,16 @@ S.D.G."""
 
 from os.path import join as pjoin
 from pathlib import Path
-from js2py import eval_js
+#from js2py import eval_js
 from . import static
 
 def is_staff(user):
     """Check if a user is channel staff"""
     return True in [badge in user.badges for badge in static.Moderation.staff_badges]
 
-def calc_password_hashes(password, salts):
-    """Hash a password given the salts"""
-    with open(pjoin(Path(__file__).parent, pjoin("dom_scripts", "md5Ex.js")), 'r') as f:
-        js = f.read()
-
-    return eval_js(js)(password, salts)
+# def calc_password_hashes(password, salts):
+#     """Hash a password given the salts"""
+#     with open(pjoin(Path(__file__).parent, pjoin("dom_scripts", "md5Ex.js")), 'r') as f:
+#         js = f.read()
+#
+#     return eval_js(js)(password, salts)
